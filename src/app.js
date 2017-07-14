0a9d0a860af07e2bc982b9981912bc18c2252d35
Marcelo Carvalho <celocarvalho7@gmail.com>
2017-07-13 21:20:50 -0300

    heuristic solution for scroll monitor

--- src/app.js
+++ src/app.js
@@ -2,16 +2,9 @@ import { combine, of, generate } from 'most'
 import { create as createStream } from '@most/create'
 import { create as createScrollMonitor, createContainer as createScrollMonitorContainer } from 'scrollmonitor'
 import Snap from 'snapsvg'
-import { compose, map, chain, pluck, identity, tap, match, apply, join, concat } from 'ramda'
+import { compose, map, prop, tryCatch, always } from 'ramda'
 
 const { svg_config, scroll_config, amoebas_config } = window;
-
-const getMinAndMax = (numbers) =>
-	[Math.min(...numbers), Math.max(...numbers)]
-const log = tap(console.log.bind(console))
-const getPathMax = compose( getMinAndMax, match(/-?(\d{1,8}(?:\.\d{1,8})?)/g), join(''), chain(identity))
-const createViewBoxDimensions = (offsetX, offsetY, [min, max]) => 
-		`${offsetX} ${offsetY} ${max-min} ${max-min}`
 
 const createPaper = (width, height, offsetX=0, offsetY=0) =>
     of({ 
@@ -50,17 +43,45 @@ const createSvgStream = ({ size: {width, height}, gradients, container}) =>
 
 const $svg = createSvgStream(svg_config)
 
+const getElementPadding = (element) => {
+	const style = getComputedStyle(element, null);
+	return {
+		top: parseInt(style.getPropertyValue('padding-top'), 10),
+		left: parseInt(style.getPropertyValue('padding-left'), 10),
+		right: parseInt(style.getPropertyValue('padding-right'), 10),
+		bottom: parseInt(style.getPropertyValue('padding-bottom'), 10)
+	}
+	
+}
+
+const getElementsPadding = compose( map(element => ({ element, padding: getElementPadding(element) })), selector => [...document.querySelectorAll(selector)])
+
+const getPadding = prop('padding')
+const getTop = tryCatch(compose(prop('top'), getPadding), always(0))
+const getBottom = tryCatch(compose(prop('bottom'), getPadding), always(0))
+
+const setOffsets = (element, index, elements) => {
+	const prev_bottom = getBottom(elements[index-1])
+	const next_top = getTop(elements[index+1])
+	const current_bottom = getBottom(element)
+	const current_top = getTop(element)
+
+	return Object.assign({}, element, { offset: { top: current_top+prev_bottom, bottom: current_bottom+next_top  } })
+}
+
 const createScrollMonitorStream = ({parent, children}) =>
     createStream(add => {
         const containerMonitor = createScrollMonitorContainer(document.querySelector(parent));
-        [...document.querySelectorAll(children)]
-            .forEach(element => {
-                containerMonitor
-                    .create(element, { bottom: - (window.innerHeight - (window.innerHeight * 0.3)), top: -(window.innerHeight * 0.3) })
-                    .fullyEnterViewport(() => {
-                        if(element.dataset.amoebaId) add(element.dataset.amoebaId)
-                    })
-            })
+        getElementsPadding(children)
+		.forEach(compose(({element, offset}) => {
+			containerMonitor
+                    		.create(element, { top: window.innerHeight * 0.7, bottom: window.innerHeight * 0.29 })
+                    		.fullyEnterViewport(function() {
+					console.log(this)
+                        		if(element.dataset.amoebaId) add(element.dataset.amoebaId)
+                    		})
+		
+		}, setOffsets))
     })
 
 const $scroll_monitor = createScrollMonitorStream(scroll_config)
@@ -99,4 +120,4 @@ const createAnimationLoop = $animation =>
         )
         .switchLatest()
 
-createAnimationLoop($animation).forEach(console.log.bind(console)).catch(console.log.bind(console))
+createAnimationLoop($animation).forEach(_=>_).catch(console.log.bind(console))
